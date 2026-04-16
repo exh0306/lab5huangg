bad904e70f17606a92116361062d2030de61802b
exh0306 <exh5452@psu.edu>
2026-04-16 13:39:30 -0400

    added sender and receiver class

---
+++ src/HMACReceiver.java
@@ -0,0 +1,31 @@
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class HMACReceiver {
+    public static void main(String[] args) throws Exception {
+
+        String key = "secret_key";
+        String message = "hello world";
+
+        String receivedHMAC = "zxpBivqvx5jfSP2ASiq/aXAoOv2MQLQfgYrZtspPjKg=";
+
+        Mac mac = Mac.getInstance("HmacSHA256");
+
+        //converts key into bytes and loads into HMAC system
+        mac.init(new SecretKeySpec(key.getBytes(), "HmacSHA256"));
+        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "HmacSHA256");
+
+        //turns message into bytes, creates the HMAC, encode the message, and turns into a string
+        String computedHMAC = Base64.getEncoder() .encodeToString(mac.doFinal(message.getBytes()));
+
+        if (computedHMAC.equals(receivedHMAC)) {
+            //Check if computed HMAC ==received HMAC
+            System.out.println("HMAC received");
+        } else {
+            System.out.println("HMAC NOT received");
+        }
+        System.out.println("Message: " + message);
+
+    }
+}
